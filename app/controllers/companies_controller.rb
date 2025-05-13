2c2c913d505db42529c4424a966b19ecdd374610
Sean Connors <sean.connors@kellogg.northwestern.edu>
2025-05-13 16:28:41 +0000

    web app new updates

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,25 +1,33 @@
 class CompaniesController < ApplicationController
 
   def index
+    @companies = Company.all 
     # find all Company rows
     # render companies/index view
   end
 
   def show
     # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    p @company 
     # render companies/show view with details about Company
+    @xontacts = Contact.where({"company_id" => @company["id"]})
   end
 
   def new
     # render view with new Company form
   end
 
-  # def create
+  def create
   #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+@company = Company.new
+@company["name"] = params["name"]
+@company["city"] = params["city"]
+@company["state"] = params["state"]
+@company.save
+
+redirect_to "/companies"
+  end
 
   # def edit
   #   # find a Company
